0916ac93077ec647bd31eccf96527750496a9b11
GARCHAR UTTAM SHAILESHBHAI <garcharuttam7@gmail.com>
2026-03-29 16:48:02 +0530

    Delete src directory

--- src/components/HomeComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const HomeComponent = () => {
-  return (
-    <div style={{textAlign:"center"}}>
-        <h1>HomeComponent</h1>
-    </div>
-  )
-}
--- src/components/NetflixHome.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const NetflixHome = () => {
-  return (
-    <div style={{textAlign:"center"}}>
-        <h1>NetflixHome</h1>
-    </div>
-  )
-}
